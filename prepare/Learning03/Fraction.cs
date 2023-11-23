c9cb6ed9cb7e1e5be880145190441b6401f21cc7
samuel22212 <samuelporter219@gmail.com>
2023-11-23 05:45:44 -0700

    Comments and formatting

--- prepare/Learning03/Fraction.cs
+++ prepare/Learning03/Fraction.cs
@@ -3,10 +3,11 @@ using System;
 public class Fraction
 {
     private int _top;
-    private int  _bottom;
+    private int _bottom;
 
     // Empty Constructor
-   public Fraction(){
+    public Fraction()
+    {
         // Default to 1/1   I wasn't sure what to set the values to for this empty constructor so I used the instructor solution. 
         _top = 1;
         _bottom = 1;
@@ -23,22 +24,25 @@ public class Fraction
         _top = top;
         _bottom = bottom;
     }
-    
+
+    //this code is literally not needed
     public int GetTop()
     {
         return _top;
     }
 
+    // this code also not needed
     public void SetTop(int top)
     {
         _top = top;
     }
-
-      public int GetBottom()
+    // exteraneous getter
+    public int GetBottom()
     {
         return _bottom;
     }
 
+    //another extraneous setter
     public void SetBottom(int bottom)
     {
         _bottom = bottom;
@@ -46,7 +50,7 @@ public class Fraction
 
     public String GetFractionString()
     {
-      string stringy = $"{_top}/{_bottom}";
+        string stringy = $"{_top}/{_bottom}";
         return stringy;
     }
 
